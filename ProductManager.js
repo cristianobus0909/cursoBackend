729f12c45ceda5d7bc1032863ec2696581f87833
cristian bustamante <clb.cristian@gmail.com>
2024-02-05 19:56:31 -0300

    segundo desafio

--- ProductManager.js
+++ ProductManager.js
@@ -1,65 +1,114 @@
+
+import * as fs from 'fs';
+
+
 class ProductManager  {
-    constructor(){
+    constructor(path){
+        this.path = path;
         this.products = [];
         this.autoIncrementId = 1;
     }
-    getProducts(){
-        return this.products;
-    }
-    addProduct(title,description,price,thumbnail,code,stock){
-        if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
-            console.log("Todos los campos son obligatorios.");
-            return;
+    //obtener o leer  el archivo de productos 
+    getProducts = async()=>{
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            return products;
+        } catch (error) {
+            console.error(error, 'No se pudo obtener ningún archivo');
+            return []
         }
+                
+        
+    }
+    //agregar un producto
+    addProduct = async(title,description,price,thumbnail,code,stock)=>{
+        try {
+            if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
+                console.log("Todos los campos son obligatorios.");
+                return;
+            }
+                
+            if (this.products.some((product) => product.code === code)) {
+                console.log(`El producto con código ${code} ya existe.`);
+                return;
+            }
+            const newProduct = {
+                id: this.autoIncrementId++,
+                title,
+                description,
+                price,  
+                thumbnail,
+                code,
+                stock,
+            };
+            this.products.push(newProduct);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2), 'utf8');
+            return `Nuevo producto agregado:${newProduct}`;
+        } catch (error) {
+            console.error(error, 'No se pudo agregar el producto')
+        }
+    }
+    //obtener producto por id
+    getProductById = async(productId)=>{
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            const productById = products.find((e)=>e.id === productId)
+            if (!productById) {
+                return console.log('No se encontró el producto');
+            }else {
+                console.log(`Producto con el Id: ${productById.id} fue encontrado exitosamente`);
+                return productById;
+            }
+        } catch (error) {
+            console.error(error, 'No se encontro el id del producto')
+        }
+        
+    }
+    //actualizar
+    updateProduct = async (id, updatedFields)=>{
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            let productsFromFile = JSON.parse(data);
+            let index = productsFromFile.findIndex((element) => element.id == id);
+            if (index === -1) {
+                console.log(`El producto con el id:${id}, es inexistente `)
+                return;
+            }
+            productsFromFile[index] = {
+                ...productsFromFile[index],
+                ...updatedFields
+            }
+            await fs.promises.writeFile(this.path, JSON.stringify(productsFromFile, null, 2), 'utf8')
+        } catch (error) {
+            console.error('No se pudo actualizar el producto')
+        }
+        
+    };
+    deleteProduct = async (id) => {
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf8');
+            let productsFromFile = JSON.parse(data);
             
-        if (this.products.some((product) => product.code === code)) {
-            console.log(`El producto con código ${code} ya existe.`);
+            const productIndex = productsFromFile.findIndex((product) => product.id === id);
+            if (productIndex === -1) {
+            console.log("Producto no encontrado.");
             return;
+            }
+            
+            productsFromFile.splice(productIndex, 1);
+            
+            fs.writeFileSync(this.path, JSON.stringify(productsFromFile, null, 2), 'utf8');
+        } catch (error) {
+            console.error("Error al leer o escribir en el archivo:", error);
         }
-        const newProduct = {
-            id: this.autoIncrementId++,
-            title,
-            description,
-            price,  
-            thumbnail,
-            code,
-            stock,
-        };
-        this.products.push(newProduct);
-
-    }
-    getProductById(productId){
-        const productById = this.products.find((e)=>e.id === productId)
-        if (!productById) {
-            return console.log('No se encontró el producto');
-        }else {
-            console.log(`Producto con el Id: ${productById.id} fue encontrado exitosamente`);
-            return productById;
-        }
-    }
+    };
 }
+const productManager = new ProductManager('./products.json')
 
-const productManager = new ProductManager()
-console.log(productManager.getProducts());
+export default productManager;
 
-productManager.addProduct(
-    "Camara Nikon",
-    "Camara Nikon reflex 24mp",
-    3000,
-    'https://i.ibb.co/ZzXvqPV/nik',
-    'CAM-01',
-    10
-)
-productManager.addProduct(
-    "Camara Cannon",
-    "Camara Canon DSLR 45mp",
-    6000,
-    'https://i.ibb.co/JxhjHgQ/c',
-    'CAN-01',
-    8
 
-)
-
-console.log(productManager.getProducts());
-console.log(productManager.getProductById(1));
 
